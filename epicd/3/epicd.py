b09aebfb9bec395292075880e672a300b05d143d
Michael Meffie <mmeffie@sinenomine.net>
2018-12-15 12:24:54 -0500

    more game server status page

--- epicd/3/epicd.py
+++ epicd/3/epicd.py
@@ -8,8 +8,18 @@ PORT = 8080
 class WebHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         print('got request', self.path)
-        uptime = self.server.game.uptime()
-        response = 'Uptime is {uptime:.2f} seconds.'.format(uptime=uptime)
+        if self.path == '/':
+            self.show_status()
+        else:
+            self.send_error(404, 'not found')
+
+    def show_status(self):
+        game = self.server.game
+        response = '<p>Uptime is {game.uptime:.2f} seconds.</p>\n'.format(game=game)
+        response += '<ul>\n'
+        for p in game.players.values():
+            response += '<li>{p.username} joined {p.joined:.2f}</li>\n'.format(p=p)
+        response += '</ul>\n'
         self.respond(response)
 
     def respond(self, response):
@@ -25,6 +35,7 @@ class GameServer:
         self.http_server = http.server.HTTPServer((HOST, PORT), WebHandler)
         self.http_server.game = self # context for handlers
 
+    @property
     def uptime(self):
         if self.started:
             return time.time() - self.started
