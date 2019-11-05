5753629911759a039a09298cc1e454bf1351c240
Michael Meffie <mmeffie@sinenomine.net>
2019-11-05 11:48:39 -0500

    wip: tcp chat demo

--- chat/chat_client.py
+++ chat/chat_client.py
@@ -1,4 +1,3 @@
-import sys
 import socket
 
 def main():
@@ -9,17 +8,14 @@ def main():
     try:
         sock.connect(address)
         while True:
-            text = input('> ')
-            if not text.endswith('\n'):
-                text += '\n'
-            message = text.encode('utf-8')
-            sock.sendall(message)
-
-            message = sock.recv(1024)
-            text = message.decode('utf-8').rstrip()
-            print('received:', text)
+            message = input('> ')
+            data = bytes(message+'\n', 'utf-8')
+            sock.sendall(data)
+            data = sock.recv(1024)
+            message = str(data, 'utf-8').rstrip()
+            print('received:', message)
     finally:
         sock.close()
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()
--- chat/chat_server.py
+++ chat/chat_server.py
@@ -1,44 +1,46 @@
 #
-# Simple chat server.
+# Simple chat server with tcp/ip sockets.
 #
 # usage: python3 chat_server.py
 #
 
 import sys
+import re
 import socketserver
 
-class MessageHandler(socketserver.StreamRequestHandler):
+class ChatServer(socketserver.StreamRequestHandler):
 
-    def read(self):
-        socket = self.rfile
-        line = socket.readline()
-        message = line.decode('utf-8')
-        return message.rstrip()
+    def read_message(self):
+        data = self.rfile.readline()
+        message = str(data, 'utf-8').rstrip()
+        return message
 
-    def write(self, message):
-        socket = self.wfile
-        if not message.endswith('\n'):
-            message += '\n'
-        line = message.encode('utf-8')
-        socket.write(line)
+    def write_message(self, message):
+        data = bytes(message+'\n', 'utf-8')
+        self.wfile.write(data)
 
     def handle(self):
+        sessions = {}
         while True:
-            message = self.read()
+            message = self.read_message()
             print(self.request.getpeername(), message)
-            self.write('you said: ' + message)
-
+            if not message:
+                continue
+            if message.startswith('/join'):
+                self.write_message('welcome')
+            else:
+                sys.stderr.write('ERROR: {0}\n'.format(message))
 
 def main():
     ip = 'localhost'
     port = 5005
     address = (ip, port)
     socketserver.TCPServer.allow_reuse_address = True
-    server = socketserver.ThreadingTCPServer(address, MessageHandler)
+    server = socketserver.ThreadingTCPServer(address, ChatServer)
     try:
         server.serve_forever()
     finally:
         server.server_close()
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()
